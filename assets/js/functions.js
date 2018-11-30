fbe3b09c36bd07cbcfc14e1f6e75eae0469ed472
Matthew Evans <Matthewevans521@gmail.com>
2018-11-29 18:35:18 -0600

    Added Individual Project Sections
    I added an animation and project sections for each of my two projects. currently they are simply sliding the information in on the x axis, and holding the content off screen, then hiding it on the way out. I will come back to this later and find a more elegant solution, but this works for now.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,9 @@
 $(function() {
     smoothScroll(350);
+    cprnmoreWrap();
+    onmaxWrap();
+    //projectWrap();
+    //test();
 });
 
 // smoothScroll function is applied from the document ready function
@@ -16,3 +20,54 @@ function smoothScroll (duration) {
 	    }
 	});
 }
+
+// Project Section Navigation and Animation (each wrap is a diffrent project)
+function cprnmoreWrap() {
+    $('.thumbnail-1').click(function() {
+        $('.projectContainer').css('left','-100%');
+        $('.cprnmoreWrap').show();
+    });
+
+    $('.projectBack').click(function() {
+        $('.projectContainer').css('left', '0%');
+        $('.cprnmoreWrap').hide(800);
+    });
+}
+
+function onmaxWrap() {
+    $('.thumbnail-2').click(function() {
+        $('.projectContainer').css('left','-100%');
+        $('.onmaxWrap').show();
+    });
+
+    $('.projectBack').click(function() {
+        $('.projectContainer').css('left', '0%');
+        $('.onmaxWrap').hide(800);
+    });
+}
+
+//Working on consolidateing the project wrap into one function below (unfinished)
+/**
+function test() {
+    $('thumbnail-#').click(function() {
+
+    $('.projectContainer').css('left','-100%');
+});
+
+
+
+function projectWrap() {
+    $('thumbnail').click(function() {
+
+        var proj =  $(this).attr('id');
+
+        $('.projectContainer').css('left','-100%');
+        $('.'(proj)'Wrap').show();
+    });
+
+    $('.projectBack').click(function() {
+        $('.projectContainer').css('left', '0%');
+        $('.' + proj + 'Wrap').hide(800);
+    });
+}
+**/
